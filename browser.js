455d28a2e05b796f91e0e4ebfe3e220de993a2c3
Brandon S <brandon.sovran@rogers.com>
2020-10-18 13:56:29 -0400

    Fix Remote Headless Chrome Sandbox

--- browser.js
+++ browser.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
 (async()=> {
-	global.browser = await puppeteer.launch({ headless: true });
+	global.browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 	console.log(`Browser set`);
 })();
