4b6ea8999c227e033f1dd4098a5b174f0913d2de
Brandon S <brandon.sovran@rogers.com>
2020-10-20 13:52:21 -0400

    Hook test

--- commands/cleanup.js
+++ commands/cleanup.js
@@ -3,7 +3,7 @@ const reply = require('./reply');
 module.exports = {
 	excluded: true,
 	name: 'cleanup',
-	description: 'Cleanup Stuff.',
+	description: 'Cleanup Stuff',
 	execute(message) {
 		//message.delete();
 		//reply.editLink(message);
