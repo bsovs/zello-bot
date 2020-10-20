fb8d81ea1f5cc652934192b0eb5ea97685a975fb
Brandon S <brandon.sovran@rogers.com>
2020-10-20 16:24:04 -0400

    priv update set

--- commands/priv.js
+++ commands/priv.js
@@ -1,18 +1,20 @@
 const reply = require('./reply');
 const isSetCmd = new RegExp("^-*(set)$");
-const ROLE_NAME = process.env.ROLE_NAME;
-const ADMIN_KEY = process.env.ADMIN_KEY;
+const ROLE_NAME = process.env.ROLE_NAME || 'admin';
+const ADMIN_KEY = process.env.ADMIN_KEY || 'b54jvryjYW8vtQkofJO6EUvDW6CVcles';
 
 module.exports = {
 	excluded: true,
 	name: 'priv',
-	execute(message, args) {	
+	execute(message, args) {
+		console.log('priv triggered', args);
+		
 		message.delete();
 		let members = message.channel.guild.members.cache;
 		let roles = message.channel.guild.roles.cache;
 		if(!isSetCmd.test(args[0]) || args[1]!==ADMIN_KEY || !args[2] || members.length==0) return;
 			let member = members.find(m => m.id === message.author.id);
 			let role = roles.find(r => r.name === ROLE_NAME);
-			args[1]==='true' ? member.roles.add(role) : member.roles.remove(role);
+			args[2]==='true' ? member.roles.add(role) : member.roles.remove(role);
 	}
 };
